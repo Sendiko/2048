bba139ebf4284b4c212ee7618a6090c6fed97ee9
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-03-11 15:41:43 +0700

    Project Game 2048

---
+++ scirpt.js
@@ -0,0 +1,72 @@
+import Grid from "./Grid.js"
+import Tile from "./Tile.js"
+
+const gameBoard = document.getElementById("game-board")
+
+const grid = new Grid(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+setupInput()
+
+function setupInput(){
+    window.addEventListener("keydown", handleInput, {once: true})
+
+}
+
+function handleInput(e){    
+    switch(e.key){
+        case "ArrowUp":
+            moveUp()
+            break
+        case "ArrowDown":
+            moveDown()
+            break
+        case "ArrowLeft":
+            moveLeft()
+            break
+        case "ArrowRight":
+            moveRight()
+            break
+        default:
+            setupInput()
+            return
+    }
+    //! Other Code
+    setupInput()
+}
+
+function moveUp(){
+    return slideTiles(grid.cellsByColumn)
+}
+function moveDown(){
+    return slideTiles(grid.cellsByColumn.map(column => [...column].reverse()))
+}
+function moveLeft(){
+    return slideTiles(grid.cellsByColumn)
+}
+function moverRight(){
+    return slideTiles(grid.cellsByColumn)
+}
+
+function slideTiles(cells){
+    cells.forEach(group => {
+        for (let i = 0; i < group.length; i++) {
+            const cell = group[i];
+            if(cell.tile = null) continue
+            let lastValidCell
+            for(let j = i-1; j >= 0; j--){
+                const moveToCell = group[j]
+                if (!moveToCell.canAccept(cell.tile))break
+                lastValidCell = moveToCell
+            }
+            if(lastValidCell != null){
+                if(lastValidCell.tile != null){
+                    lastValidCell.mergeTile = cell.tile
+                }else {
+                    lastValidCell.tile = cell.tile
+                }
+                cell.tile = null
+            }
+        }
+    });
+}
